8ace8b5ba8ed815fa8659ead0213ddb1a51a0975
akiasa oshima <asaolr4@gmail.com>
2022-10-15 22:37:33 +0900

    ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  skip_before_action :login_required, only: [:new, :create]
 
   def index
     @users = User.all
@@ -43,6 +44,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :image)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
     end
 end
