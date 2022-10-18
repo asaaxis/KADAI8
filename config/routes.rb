a9499b0cc31dd7b6eb2b6cea12662ed62a45fd72
akiasa oshima <asaolr4@gmail.com>
2022-10-18 11:05:00 +0900

    お気に入りと一覧表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   resources :users
 
   resources :pictures do
